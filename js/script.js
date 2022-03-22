d4540cfae2b630a3911c141ec44efdf04421c52b
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-03-22 21:56:02 +0200

    js code was moved to js file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log(1)
+    // current year for the copyright
+    $('#year').text(new Date().getFullYear());
+    // <span id='year'></span>
+
+    //init Scrollspy
+    $('body').scrollspy({target: '#main-nav'}); 
+    // I dont actually need it here. It works perfectly fine without it
+
+
+        //add smooth scrolling
+        $('#main-nav a').on('click', function(e){
+      //check for hash value 
+      if(this.hash !== ''){
+        //Prevent default behavior
+        e.preventDefault();
+
+        // store hash
+        const hash = this.hash;
+
+        //Animate smooth scrolling
+        $('html, body').animate({
+           scrollTop: $(hash).offset().top
+        }, 100, function(){
+          // Add hash to URL after scroll
+          window.location.hash = hash;
+        });
+      }
+    })
